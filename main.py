9c192d603e1f8befe3e9535cfde3fe1382bbc336
Monirul Middya <=>
2023-12-24 04:07:15 +0530

    update

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ st.title("QA from CSV")
 btn = st.button("Create Knowledgebase")
 if btn:
     create_vector_db()
+    
+st.markdown(""" Example questions: 
+            ( should I learn power bi or tableau? , I've a MAC computer. Can I use powerbi on it? ,do you have javascript course? )""")
 
-question = st.text_input("Question: ")
+question = st.text_input("Question: ", placeholder="Write Like: What is your refund policy?")
 
 if question:
     chain = get_qa_chain()
